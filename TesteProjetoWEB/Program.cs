c47a9569d6910767b38d4a5385785dea890e1ecc
Lu4nScr1pt1ng <rd1qys@gmail.com>
2022-12-14 19:34:04 -0300

    cors to react app in vite

--- TesteProjetoWEB/Program.cs
+++ TesteProjetoWEB/Program.cs
@@ -1,12 +1,25 @@
 using TesteProjetoWEB.Endpoints;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://127.0.0.1:5173").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.AddPersistence();
 
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapCepEndpoints();
 
